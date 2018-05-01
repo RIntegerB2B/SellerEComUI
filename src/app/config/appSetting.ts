f077068fa93609e8f7a8b28bc3bbc4f30097d546
Chandran <Preveen_Chandran@Dell.com>
2018-05-01 18:42:41 +0530

    Server URL Change

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -3,8 +3,8 @@
     awsServiceUrl : "http://ec2-13-127-168-120.ap-south-1.compute.amazonaws.com:4200/"
 } */
 
-export const AppSetting : AppSettingType = {
-    awsServiceUrl:"http://ec2-13-127-165-9.ap-south-1.compute.amazonaws.com:3006/",
-    local3000ServiceUrl:"http://localhost:3006/",
-    serviceUrl:"ec2-13-127-137-140.ap-south-1.compute.amazonaws.com:3006/"
-}
+export const AppSetting: AppSettingType = {
+    awsServiceUrl: 'http://http://ec2-13-127-198-141.ap-south-1.compute.amazonaws.com:3006/',
+    local3000ServiceUrl: 'http://localhost:3006/',
+    serviceUrl: 'http://ec2-13-127-198-141.ap-south-1.compute.amazonaws.com:3006/'
+};
